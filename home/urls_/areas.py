619b2703b0d91c1f218a8ef9921be0ecd13d7921
Ricardo <ricardoqb95@gmail.com>
2021-01-15 16:55:23 -0600

    informatica

--- home/urls_/areas.py
+++ home/urls_/areas.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from home.views_.areas import finanzas_view, planeacion_view, academica_view, vinculacion_view, normatividad_view
+from home.views_.areas import finanzas_view, planeacion_view, academica_view, vinculacion_view, normatividad_view, informatica_view
 
 appname = "home"
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('areas/academica', academica_view, name="area-academica"),
     path('areas/vinculacion', vinculacion_view, name="area-vinculacion"),
     path('areas/normatividad', normatividad_view, name="area-normatividad"),
+    path('areas/informatica', informatica_view, name="area-informatica"),
 ]
