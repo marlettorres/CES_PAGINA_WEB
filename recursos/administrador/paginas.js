3340ed67b990ea11dc16ff515356f1ef2ec24528
Eugenio <egenio.21@gmail.com>
2022-06-24 14:00:23 -0500

    modificacion js

--- recursos/administrador/paginas.js
+++ recursos/administrador/paginas.js
@@ -4,11 +4,13 @@ let bloque_imagen = document.getElementById("bloque-imagen");
 let bloque_pdf = document.getElementById("bloque-pdf");
 
 let bloque_descripcion = document.getElementById("bloque-descripcion");
+let bloque_video = document.getElementById("bloque-video");
 
 
 let ocultar_div = (e) => {
   bloque_texto.style.display = "none";
   bloque_imagen.style.display = "none";
+  bloque_pdf.style.display = "none";
   bloque_pdf.style.display = "none";
   if (e.target.value === "1") bloque_texto.style.display = "block";
   if (e.target.value === "2") bloque_pdf.style.display = "block";
@@ -23,12 +25,15 @@ $gmx(document).ready(function(){
   var tipo=  selection.options[selection.selectedIndex].value;
   if(tipo==6){
     bloque_descripcion.style.display = "block";
+    bloque_video.style.display = "block";
   }
   $("#pagina_cecyte_id").change(function(){
     bloque_descripcion.style.display = "none";
+    bloque_video.style.display = "none";
     var tipo = $("#pagina_cecyte_id").val();
     if(tipo==6){
       bloque_descripcion.style.display = "block";
+      bloque_video.style.display = "block";
     }
   });
 
